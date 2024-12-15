1a84d3e80881c3ec91780ba15e7f52905fbe6a29
mickael59b <mboutte621@gmail.com>
2024-12-15 09:27:17 +0100

    Fix package-lock.json and node_modules

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/mon-portfolio/',  // Ajoutez cette ligne pour spécifier le sous-dossier sur GitHub Pages
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Assurez-vous que Vercel génère dans le bon dossier
+  },
 });
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> 985ed9990ff16ed45eafce5745f39d8010d1c359
