fed8264cfad22e3df41c2d8da8ac21c6c1ed9dfe
mickael59b <mboutte621@gmail.com>
2024-12-15 07:30:09 +0100

    Déploiement du projet sur gh-pages

--- front-end/monportfolio/src/context/useAuth.js
+++
@@ -1,9 +0,0 @@
-// src/context/useAuth.js
-import { useContext } from 'react';
-import { AuthContext } from './AuthContext';
-
-const useAuth = () => {
-  return useContext(AuthContext);
-};
-
-export default useAuth;
--- front-end/monportfolio/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/mon-portfolio/',  // Ajoutez cette ligne pour spécifier le sous-dossier sur GitHub Pages
-  plugins: [react()],
-});
